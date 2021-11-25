180d708fab70274bcd76ebd26e547fee25684221
Geerocktricks <geerockface4@gmail.com>
2021-11-25 12:25:51 -0800

    Adds a 404 page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
