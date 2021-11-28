397ef8635cec2bb98005a6bb2011ea8c0b27bd3c
Geerocktricks <geerockface4@gmail.com>
2021-11-28 10:32:51 -0800

    Generates resume, portfolio and contact components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,19 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { ResumeComponent } from './components/resume/resume.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     AboutMeComponent,
-    PageNotFoundComponent, 
+    PageNotFoundComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,6 +31,7 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
     RouterModule.forRoot([
       {path: 'home-page', component: HomePageComponent},
       {path: 'about-me', component: AboutMeComponent},
+      {path: 'resume', component:ResumeComponent},
       {path: '', redirectTo: '/home-page', pathMatch: 'full'},
       {path: '**', component: PageNotFoundComponent},
     ]),
