d6025f3017054e18af156b1b6cbc852c692f763b
Geerocktricks <geerockface4@gmail.com>
2021-11-30 05:04:11 -0800

    Creates gitHubService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
@@ -42,7 +43,8 @@ import { GitCloneComponent } from './components/git-clone/git-clone.component';
     ]),
     FontAwesomeModule,
     BrowserAnimationsModule,
-    CarouselModule 
+    CarouselModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/git-clone/git-clone.component.ts
+++ src/app/components/git-clone/git-clone.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-git-clone',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GitCloneComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('https://api.github.com/users/geerocktricks')
+    .subscribe(data =>{
+      console.log(data);
+    })
   }
 
 }
